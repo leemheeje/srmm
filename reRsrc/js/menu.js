4af0ff51eb55cead218628d42a50d882a69ca8aa
leemheeje <dlagmleo89@gmail.com>
2017-09-05 18:43:43 +0900

    menu.js 적용.

--- reRsrc/js/menu.js
+++ reRsrc/js/menu.js
@@ -1,36 +1,61 @@
-
-var MENU = [[["창의체험활동존","http://www.crezone.net/?page_id=300"],
-[["창의적체험활동 소개", "http://www.crezone.net/?page_id=300"],
-["창의체험자원", "http://www.crezone.net/?resourcecat=%EC%B2%B4%ED%97%98%EC%8B%9C%EC%84%A4"],
-["창의체험 프로그램", "http://www.crezone.net/?programcat=%EC%B0%BD%EC%9D%98%EC%A0%81%EC%B2%B4%ED%97%98%ED%99%9C%EB%8F%99"],
-["창의체험 수업모델", "http://www.crezone.net/?crmodelcat=%ec%88%98%eb%8f…%b4%ed%97%98-%ec%88%98%ec%97%85%eb%aa%a8%eb%8d%b8"],
-["창의체험 큐레이션", "/?page_id=424070&m=9"],
-["이슈캘린더", "http://www.crezone.net/?issuecat=9%ec%9b%94"],
-["전국교육청 창의체험자원지도", "/?page_id=428614"]]],
-[["수학여행존New","http://www.crezone.net/?page_id=153545"],
-[["수학여행 모델", "http://www.crezone.net/?page_id=153545"],
-["시도교육청 현장학습 공개방", "http://www.crezone.net/?page_id=152872"],
-["현장학습 자료실", "http://www.crezone.net/?page_id=433738"],
-["수학여행 이야기", "http://www.crezone.net/?page_id=518724"],
-["안전요원 구인/구직New", "http://www.crezone.net/?page_id=126126"]]],
-[["창의교육존","http://www.crezone.net/?page_id=3633"],
-[["창의교육 수업모델", "/?page_id=3633"],
-["지속가능발전교육", "/?crfile_cat=지속가능발전교육-수업모델"],
-["연구회 커뮤니티", "/?page_id=235309"],
-["연구회 공개 자료방", "http://www.crezone.net/?page_id=510158"],
-["창의교육 교원연수", "http://www.crezone.net/?page_id=490309"],
-["창의교육 거점센터", "http://www.crezone.net/?page_id=447628"],
-["창의교육 선도 교원양성대학", "http://www.crezone.net/?page_id=424573"],
-["창의교육 질의응답", "/?page_id=126176&ctype=L&R=Y&C=all"],
-["글로벌 창의포럼", "http://www.crezone.net/?page_id=502579&m=V&search_word=&n=102"]]],
-[["크레존 블로그New","http://www.crezone.net/?page_id=125102"],
-[["해외창의 이야기New", "http://www.crezone.net/?page_id=125102"],
-["행복학교 이야기New", "http://www.crezone.net/?page_id=125052"],
-["창의교육 이야기", "http://www.crezone.net/?page_id=127718"],
-["테마여행 이야기", "http://www.crezone.net/?page_id=514204"]]],
-[["크레존 뉴스New","/?page_id=124865"],
-[["뉴스레터 New", "http://www.crezone.net/?page_id=124865"],
-["카드뉴스", "http://www.crezone.net/?page_id=425864"]]],
-[["자료실","http://www.crezone.net/?page_id=163"],
-[["문서 자료실", "http://www.crezone.net/?page_id=163"],
-["멀티미디어 자료실", "http://www.crezone.net/?ebook_cat=%eb%a9%80%ed%8b%…%b8%eb%94%94%ec%96%b4-%ec%9e%90%eb%a3%8c%ec%8b%a4"]]],]
+var MENU = [
+    [
+        ["창의체험활동존", "http://www.crezone.net/?page_id=300"],
+        [
+            ["창의적체험활동 소개", "http://www.crezone.net/?page_id=300"],
+            ["창의체험자원", "http://www.crezone.net/?resourcecat=%EC%B2%B4%ED%97%98%EC%8B%9C%EC%84%A4"],
+            ["창의체험 프로그램", "http://www.crezone.net/?programcat=%EC%B0%BD%EC%9D%98%EC%A0%81%EC%B2%B4%ED%97%98%ED%99%9C%EB%8F%99"],
+            ["창의체험 수업모델", "http://www.crezone.net/?crmodelcat=%ec%88%98%eb%8f…%b4%ed%97%98-%ec%88%98%ec%97%85%eb%aa%a8%eb%8d%b8"],
+            ["창의체험 큐레이션", "/?page_id=424070&m=9"],
+            ["이슈캘린더", "http://www.crezone.net/?issuecat=9%ec%9b%94"],
+            ["전국교육청 창의체험자원지도", "/?page_id=428614"]
+        ]
+    ],
+    [
+        ["수학여행존New", "http://www.crezone.net/?page_id=153545"],
+        [
+            ["수학여행 모델", "http://www.crezone.net/?page_id=153545"],
+            ["시도교육청 현장학습 공개방", "http://www.crezone.net/?page_id=152872"],
+            ["현장학습 자료실", "http://www.crezone.net/?page_id=433738"],
+            ["수학여행 이야기", "http://www.crezone.net/?page_id=518724"],
+            ["안전요원 구인/구직New", "http://www.crezone.net/?page_id=126126"]
+        ]
+    ],
+    [
+        ["창의교육존", "http://www.crezone.net/?page_id=3633"],
+        [
+            ["창의교육 수업모델", "/?page_id=3633"],
+            ["지속가능발전교육", "/?crfile_cat=지속가능발전교육-수업모델"],
+            ["연구회 커뮤니티", "/?page_id=235309"],
+            ["연구회 공개 자료방", "http://www.crezone.net/?page_id=510158"],
+            ["창의교육 교원연수", "http://www.crezone.net/?page_id=490309"],
+            ["창의교육 거점센터", "http://www.crezone.net/?page_id=447628"],
+            ["창의교육 선도 교원양성대학", "http://www.crezone.net/?page_id=424573"],
+            ["창의교육 질의응답", "/?page_id=126176&ctype=L&R=Y&C=all"],
+            ["글로벌 창의포럼", "http://www.crezone.net/?page_id=502579&m=V&search_word=&n=102"]
+        ]
+    ],
+    [
+        ["크레존 블로그New", "http://www.crezone.net/?page_id=125102"],
+        [
+            ["해외창의 이야기New", "http://www.crezone.net/?page_id=125102"],
+            ["행복학교 이야기New", "http://www.crezone.net/?page_id=125052"],
+            ["창의교육 이야기", "http://www.crezone.net/?page_id=127718"],
+            ["테마여행 이야기", "http://www.crezone.net/?page_id=514204"]
+        ]
+    ],
+    [
+        ["크레존 뉴스New", "/?page_id=124865"],
+        [
+            ["뉴스레터 New", "http://www.crezone.net/?page_id=124865"],
+            ["카드뉴스", "http://www.crezone.net/?page_id=425864"]
+        ]
+    ],
+    [
+        ["자료실", "http://www.crezone.net/?page_id=163"],
+        [
+            ["문서 자료실", "http://www.crezone.net/?page_id=163"],
+            ["멀티미디어 자료실", "http://www.crezone.net/?ebook_cat=%eb%a9%80%ed%8b%…%b8%eb%94%94%ec%96%b4-%ec%9e%90%eb%a3%8c%ec%8b%a4"]
+        ]
+    ],
+]
